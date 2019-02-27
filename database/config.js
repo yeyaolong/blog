c84262f993966600f982f2672705216073885b4a
yaolong <470080502@qq.com>
2019-02-27 19:39:31 +0800

    change the mongodb to mysql

--- database/config.js
+++ database/config.js
@@ -1,11 +1,10 @@
-const DB_URL = 'mongodb://localhost:27017/mongoosesample';
+/* 数据库相应信息 */
+const DB = {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "yaolong12345",
+    database: "blog"
+}
 
-var mongoose = require('mongoose');
-
-/*  连接*/
-mongoose.connect(DB_URL);
-
-/* 连接成功 */
-mongoose.connection.on('connected', function () {
-   console.log('Mongoose connection open to');
-});
+module.exports = DB;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
+var mysql = require('../database/database');
+var crypto = require('crypto');
 var express = require('express');
 var router = express.Router();
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +12,35 @@ router.get('/', function(req, res, next) {
 
 /* 登录页 */
 router.get('/login', function (req, res, next) {
-  res.render('login', {});
+  res.render('login', {message: ''});
+});
+
+router.post('/login', function (req, res, next) {
+    var name = req.body.name;
+    var password = req.body.name;
+    var hash = crypto.createHash('md5');
+    hash.update(password);
+    password = hash.digest('hex');
+    var query = 'SELECT * FROM author WHERE authorName=' + mysql.escape(name) + ' AND authorPassword=' + mysql.escape(password);
+    // var query = `SELECT * FROM author WHERE authorName=  ${name} AND authorPassword= ${password}`
+    console.log(query);
+    mysql.query(query, function (err, rows, fields) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        var user = rows[0];
+        if (user) {
+            // 登录成功
+            // console.log('req.sessioin', req.session)
+            // req.session.userSign = true
+            // req.session.userID = user.authorID;
+            res.redirect('/');
+        } else {
+            // 登录失败
+            res.render("login", {"code": 200, "message": "用户名或密码错误", "data": {}})
+        }
+    });
 });
 
 module.exports = router;
